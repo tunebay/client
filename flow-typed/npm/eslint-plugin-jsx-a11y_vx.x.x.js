42c8bb3d68d457f1b3f16731892bd19f11199376
Mali Michael <malimccalla@gmail.com>
2017-12-28 18:05:26 +0000

    Flow-typed

--- flow-typed/npm/eslint-plugin-jsx-a11y_vx.x.x.js
+++ flow-typed/npm/eslint-plugin-jsx-a11y_vx.x.x.js
@@ -1,4 +1,4 @@
-// flow-typed signature: 7b53b8eae60b8f454394b728b15e4fea
+// flow-typed signature: 13646e56366b40e78ce44ced5812a254
 // flow-typed version: <<STUB>>/eslint-plugin-jsx-a11y_v^6.0.2/flow_v0.60.0
 
 /**
@@ -199,6 +199,18 @@ declare module 'eslint-plugin-jsx-a11y/__tests__/src/util/getTabIndex-test' {
 }
 
 declare module 'eslint-plugin-jsx-a11y/__tests__/src/util/hasAccessibleChild-test' {
+  declare module.exports: any;
+}
+
+declare module 'eslint-plugin-jsx-a11y/__tests__/src/util/implicitRoles/input-test' {
+  declare module.exports: any;
+}
+
+declare module 'eslint-plugin-jsx-a11y/__tests__/src/util/implicitRoles/menu-test' {
+  declare module.exports: any;
+}
+
+declare module 'eslint-plugin-jsx-a11y/__tests__/src/util/implicitRoles/menuitem-test' {
   declare module.exports: any;
 }
 
@@ -725,6 +737,15 @@ declare module 'eslint-plugin-jsx-a11y/__tests__/src/util/getTabIndex-test.js' {
 }
 declare module 'eslint-plugin-jsx-a11y/__tests__/src/util/hasAccessibleChild-test.js' {
   declare module.exports: $Exports<'eslint-plugin-jsx-a11y/__tests__/src/util/hasAccessibleChild-test'>;
+}
+declare module 'eslint-plugin-jsx-a11y/__tests__/src/util/implicitRoles/input-test.js' {
+  declare module.exports: $Exports<'eslint-plugin-jsx-a11y/__tests__/src/util/implicitRoles/input-test'>;
+}
+declare module 'eslint-plugin-jsx-a11y/__tests__/src/util/implicitRoles/menu-test.js' {
+  declare module.exports: $Exports<'eslint-plugin-jsx-a11y/__tests__/src/util/implicitRoles/menu-test'>;
+}
+declare module 'eslint-plugin-jsx-a11y/__tests__/src/util/implicitRoles/menuitem-test.js' {
+  declare module.exports: $Exports<'eslint-plugin-jsx-a11y/__tests__/src/util/implicitRoles/menuitem-test'>;
 }
 declare module 'eslint-plugin-jsx-a11y/__tests__/src/util/isAbstractRole-test.js' {
   declare module.exports: $Exports<'eslint-plugin-jsx-a11y/__tests__/src/util/isAbstractRole-test'>;
