8bc67635fde16cd8881e4ec4ad47cad41e8cd4ff
Mali Michael <malimccalla@gmail.com>
2017-12-31 16:36:11 +0000

    Adjust navlink font size due to font change

--- src/components/common/NavLink.js
+++ src/components/common/NavLink.js
@@ -12,7 +12,7 @@ const NavLink = styled.a`
   color: ${props => (props.color ? props.color : props.theme.black)};
   padding-left: ${props => (props.spacing ? props.spacing : '3rem')};
 
-  font-size: 1.4rem;
+  font-size: 1.3rem;
   font-weight: 800;
   line-height: 3rem;
 
