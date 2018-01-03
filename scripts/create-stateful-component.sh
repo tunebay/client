4f5400c686bfcbca4ef17c21c231ea761b9091a5
Mali Michael <malimccalla@gmail.com>
2018-01-03 13:54:12 +0000

    Add create-stateful-component script

---
+++ scripts/create-stateful-component.sh
@@ -0,0 +1,74 @@
+#!/usr/bin/env bash
+
+if [ $# -ne 1 ]; then
+  echo "usage: create-stateful-component <ComponentName>"
+  exit 1
+fi
+
+ComponentName=$1
+
+RelativeProjectRoot="`dirname $0`/.."
+ProjectRoot="`cd $RelativeProjectRoot; pwd`"
+
+ComponentRoot="$ProjectRoot/src/components/$ComponentName"
+mkdir -p $ComponentRoot
+pushd $ComponentRoot
+
+
+cat << EOF > index.js
+// @flow
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+
+import { actions, type $(echo $ComponentName)State } from './state'
+
+type Props = $(echo $ComponentName)State & typeof actions
+
+class $ComponentName extends Component<Props, void> {
+  render() {
+    return (
+      <View>
+        <AppText>$ComponentName</AppText>
+      </View>
+    )
+  }
+}
+
+const mapStateToProps = (state) => ({}) // TODO
+export default connect(mapStateToProps, actions)($ComponentName)
+EOF
+
+cat <<EOF > state.js
+// @flow
+
+export type $(echo $ComponentName)Action = TodoAction
+type TodoAction = { type: '$ComponentName/todo' }
+
+export const actions = {
+  todo(): TodoAction {
+    return { type: '$ComponentName/todo' }
+  },
+}
+
+export type $(echo $ComponentName)State = {
+
+}
+
+const initialState: $(echo $ComponentName)State = {
+
+}
+
+export default function reducer(
+  state: $(echo $ComponentName)State = initialState,
+  action: $(echo $ComponentName)Action,
+): $(echo $ComponentName)State {
+  switch (action.type) {
+    case '$ComponentName/todo':
+      return { ...state, /* TODO */ }
+    default:
+      return state
+  }
+}
+EOF
+
+popd
